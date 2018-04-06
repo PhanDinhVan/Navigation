ddc07c619da4e5d168f8d0c110884a460ac18b1d
PhanDinhVan <pdvan.it@gmail.com>
2018-04-07 01:40:10 +0700

    design Home page

--- src/components/Home/ContentHome/ContentHome.js
+++ src/components/Home/ContentHome/ContentHome.js
@@ -1,21 +1,63 @@
 import React from 'react';
 
 import classes from './ContentHome.css';
-import { 
-    Tabs, 
-    Tab } from "react-bootstrap";
+import { Grid, Row, Col, Thumbnail, Carousel, Label } from "react-bootstrap";
+
+import image1 from '../../../assets/images/private_sale/image1.jpg';
+import image2 from '../../../assets/images/private_sale/image2.jpg';
+import image3 from '../../../assets/images/private_sale/image3.jpg';
+import image4 from '../../../assets/images/private_sale/image4.jpg';
+import image5 from '../../../assets/images/private_sale/image5.jpg';
 
 const contentHome = (props) => (
 
-    <div className={classes.ContentHome} >
-        <Tabs defaultActiveKey={1} animation={false} id="noanim-tab-example">
-            <Tab eventKey={1} title="PRIVATE SALES" className={classes.Tab} >
-                PRIVATE SALES
-            </Tab>
-            <Tab eventKey={2} title="NEWS" className={classes.Tab} >
-                NEWS
-            </Tab>
-        </Tabs>
+    <div>
+        <Grid bsClass={classes.Grid}>
+            <Row>
+                <Col md={4}>
+                    <Thumbnail src={image1} className={classes.Thumbnail} >
+                        
+                    </Thumbnail>
+                    
+                    
+                </Col>
+                <Col md={4}>
+                    <Thumbnail src={image2} className={classes.Thumbnail} >
+                        
+                    </Thumbnail>
+                    <Carousel.Caption bsClass={classes.Carousel} >
+                        <div className={classes.Bottomleft}>
+                            <Label bsClass={classes.Label} > registered </Label>
+                        </div>
+                    </Carousel.Caption>
+                </Col>
+                <Col md={4}>
+                    <Thumbnail src={image3} className={classes.Thumbnail} >
+                        
+                    </Thumbnail>
+                    <Carousel.Caption bsClass={classes.Carousel} >
+                        <div className={classes.Bottomleft}>
+                            <Label bsClass={classes.Label} > be notified </Label>
+                        </div>
+                    </Carousel.Caption>
+                </Col>
+                <Col md={4}>
+                    <Thumbnail src={image4} className={classes.Thumbnail} >
+                        
+                    </Thumbnail>
+                    <Carousel.Caption bsClass={classes.Carousel} >
+                        <div className={classes.Bottomleft}>
+                            <Label bsClass={classes.Label} > be notified </Label>
+                        </div>
+                    </Carousel.Caption>
+                </Col>
+                <Col md={4}>
+                    <Thumbnail src={image5} className={classes.Thumbnail} >
+                        
+                    </Thumbnail>
+                </Col>
+            </Row>
+        </Grid>
     </div>
     
 ); 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 
+import Auxs from '../../hoc/Auxs';
 import Menu from './Menu/Menu';
+import classes from './Home.css';
+import { Tabs, Tab } from "react-bootstrap";
 import ContentHome from './ContentHome/ContentHome';
 
 class Home extends Component {
     render () {
         return ( 
-            <div>
+            <Auxs>
                 <Menu />
-                <ContentHome />
-            </div>
+                {/* <ContentHome /> */}
+                <div className={classes.Home} >
+                    <Tabs defaultActiveKey={1} animation={false} id="home" >
+                        <Tab eventKey={1} title="PRIVATE SALES" >
+                            <ContentHome />
+                        </Tab>
+                        <Tab eventKey={2} title="NEWS" >
+                            NEWS
+                        </Tab>
+                    </Tabs>
+                </div>
+            </Auxs>
         );
     }
 }
